3196aa980ad3fb6a7e49b0161dde619404c8d089
Alessandro Fernandes <alcfernandes@yahoo.com>
2018-06-20 14:19:03 -0300

    Get the Github issues and displays in a table.

--- src/components/GithubIssues.vue
+++ src/components/GithubIssues.vue
@@ -16,20 +16,72 @@
         </v-flex>
 
         <v-flex xs4>
-          <v-btn color="success">Go</v-btn>
-          <v-btn color="error">Limpar</v-btn>
+          <v-btn @click.prevent.stop="getIssues()" color="success">Go</v-btn>
+          <v-btn @click.prevent.stop="fieldsReset()" color="error">Limpar</v-btn>
         </v-flex>
       </v-layout>
+      <v-data-table
+        :headers="headers"
+        :items="issues"
+        :pagination.sync="pagination"
+        class="elevation-1"
+        rows-per-page-text="Linhas por página"
+      >
+        <template slot="items" slot-scope="props">
+          <td>{{ props.item.number }}</td>
+          <td class="text-xs-left">{{ props.item.title }}</td>
+        </template>
 
+        <template slot="no-data">
+          <v-alert :value="true" color="info" icon="warning">
+            Parece que não há issues nesse repo.
+          </v-alert>
+        </template>
+
+      </v-data-table>
     </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data: () => ({
     username: '',
-    repository: ''
-  })
+    repository: '',
+    issues: [],
+    headers: [
+      {text: 'Número', value: 'number'},
+      {text: 'Título', value: 'title'}
+    ],
+    pagination: {
+      sortBy: 'number'
+    }
+  }),
+
+  methods: {
+    fieldsReset () {
+      this.username = ''
+      this.repository = ''
+    },
+
+    getIssues () {
+      const url = `https://api.github.com/repos/${this.username}/${this.repository}/issues`
+
+      axios.get(url)
+        .then((response) => {
+          this.issues = response.data
+        })
+        .catch((error) => {
+          console.log(error.response.data)
+          // this.response.status = 'error'
+          // this.response.message = 'Repositório não existe!'
+        })
+        .finally(() => {
+          // this.loader.getIssues = false
+        })
+    }
+  }
 }
 </script>
 
